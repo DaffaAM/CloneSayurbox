f63a0d8dd5aa97a874c3577b5d0372e9b3a45f51
harkon666 <bryan.wicaksanaa@gmail.com>
2020-04-30 15:51:07 +0700

    create reducers for categories

--- src/_actions/categories.js
+++ src/_actions/categories.js
@@ -1,8 +1,9 @@
 import {API} from '../config/api';
+import {GET_CATEGORY} from '../config/constants';
 
 export const get_category = () => {
   return {
-    type: 'GET_CATEGORY',
+    type: GET_CATEGORY,
     paylaod: async () => {
       const res = API.get('/category');
       return res.data;

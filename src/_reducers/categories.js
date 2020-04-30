97967698a005f47b4764d58bf852c5f609e074d6
harkon666 <bryan.wicaksanaa@gmail.com>
2020-04-30 15:53:06 +0700

    creating auth

--- src/_reducers/categories.js
+++ src/_reducers/categories.js
@@ -29,3 +29,5 @@ const reducer = (state = initialState, action) => {
       return state;
   }
 };
+
+export default reducer;
--- src/_redux/store.js
+++ src/_redux/store.js
@@ -1,9 +1,11 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux';
 import {logger, promise} from '../middleware/index';
 import categories from '../_reducers/categories';
+import auth from '../_reducers/auth';
 
 const rootReducers = combineReducers({
   categories,
+  auth,
 });
 
 const store = createStore(rootReducers, applyMiddleware(logger, promise));
